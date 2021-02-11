c7cb3eb74bc1cdc8c3fc93e173883eb2498e2522
sakithya <sakithya.chidambaram@syncfusion.com>
2021-02-11 10:31:27 +0530

    DataForm Add or Remove the Datafield

---
+++ DataformXamarin/DataformXamarin/DataformXamarin/Behavior/DataformBehavior.cs
@@ -0,0 +1,51 @@
+﻿using Syncfusion.XForms.DataForm;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DataformXamarin
+{
+    public class DataformBehavior : Behavior<ContentPage>
+    {
+        bool refreshLayout = false;
+        SfDataForm dataForm;
+        Button button;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            dataForm = bindable.FindByName<SfDataForm>("dataForm");
+            button = bindable.FindByName<Button>("Commit");
+            dataForm.DataObject = new ContactInfo();
+            dataForm.AutoGeneratingDataFormItem += DataForm_AutoGeneratingDataFormItem;
+            button.Clicked += Button_Clicked;
+        }
+        private void DataForm_AutoGeneratingDataFormItem(object sender, AutoGeneratingDataFormItemEventArgs e)
+        {
+            if (e.DataFormItem != null)
+            {
+                if (!refreshLayout)
+                {
+                    if (e.DataFormItem.Name.Equals("MiddleName") || e.DataFormItem.Name.Equals("LastName"))
+                        e.Cancel = true;
+                }
+                else if (e.DataFormItem.Name == "GroupName")
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            refreshLayout = true;
+            dataForm.RefreshLayout(true);
+        }
+    }
+}
+    
+    
+
+   
+
